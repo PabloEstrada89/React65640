a2539dc1f3e42bf698f7cf3c4ea18e63a8c0dd9f
PabloEstrada89 <pablo.estrada.89@gmail.com>
2025-01-09 08:25:12 -0300

    ProyectoFinal+Estrada5

--- asdf3D/src/Context/CartContext.jsx
+++ asdf3D/src/Context/CartContext.jsx
@@ -1,21 +1,16 @@
 import React, { createContext, useContext, useState } from "react";
 
-// Crear el contexto
 const CartContext = createContext();
 
-// Hook personalizado para acceder al contexto
 export const useCart = () => useContext(CartContext);
 
-// Proveedor del contexto
 export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
 
-    // Agregar un producto al carrito
     const addToCart = (product, quantity) => {
         const existingProduct = cart.find((item) => item.id === product.id);
 
         if (existingProduct) {
-            // Si el producto ya está en el carrito, actualizar la cantidad
             setCart(
                 cart.map((item) =>
                     item.id === product.id
@@ -24,23 +19,18 @@ export const CartProvider = ({ children }) => {
                 )
             );
         } else {
-            // Si el producto no está en el carrito, agregarlo
             setCart([...cart, { ...product, quantity }]);
         }
     };
 
-    // Calcular el total de productos en el carrito
     const totalItems = () => cart.reduce((acc, item) => acc + item.quantity, 0);
 
-    // Calcular el total del precio
     const totalPrice = () => cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
-    // Eliminar un producto del carrito
     const removeFromCart = (id) => {
         setCart(cart.filter((item) => item.id !== id));
     };
 
-    // Vaciar el carrito
     const clearCart = () => setCart([]);
 
     return (
